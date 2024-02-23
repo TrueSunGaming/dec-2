805a495912914a7bb9b28686489dce480d35700f
TrueSunGaming <truesungaming@gmail.com>
2024-02-23 02:33:16 +0000

    add macro compilation

--- src/lib/compiler.ts
+++ src/lib/compiler.ts
@@ -4,25 +4,47 @@ import { desmosFormat } from "./desmosFormat";
 import { generateTokens } from "./lexer";
 import { createSyntaxTree } from "./parser";
 
-export function compile(ast: AST): string {
+const macros: Map<string, [string[], AST]> = new Map();
+
+function buildMacro(name: string, params: AST[]): AST {
+    const res: AST = structuredClone(macros.get(name)![1]);
+    const paramMap: Map<string, AST> = new Map(params.map((v, i) => [
+        macros.get(name)![0][i],
+        v
+    ]));
+
+    for (let i = 0; i < res.parts.length; i++) {
+        if (res.parts[i].type != ASTType.Access || !paramMap.has(res.parts[i].value ?? "")) continue;
+
+        res.parts[i] = paramMap.get(res.parts[i].value!)!;
+    }
+
+    return res;
+}
+
+export function compile(ast: AST, first = false): string {
+    if (first) macros.clear();
+
     switch(ast.type) {
         case ASTType.Program:
-            return ast.parts.map(compile).join("\n");
+            return ast.parts.map((v) => compile(v)).join("\n");
 
         case ASTType.Access:
             return desmosFormat(ast.value ?? "");
 
         case ASTType.Call:
-            if (!stdlib.has(ast.value ?? "")) return `${desmosFormat(ast.value ?? "")}(${ast.parts.map(compile).join(",")})`;
+            if (!stdlib.has(ast.value ?? "") && !macros.has(ast.value ?? "")) return `${desmosFormat(ast.value ?? "")}(${ast.parts.map((v) => compile(v)).join(",")})`;
 
-            return (() => {
+            if (stdlib.has(ast.value ?? "")) return (() => {
                 let res: string = stdlib.get(ast.value!)!;
                 for (let i = 0; i < ast.parts.length; i++) res = res.replaceAll(`%%${i}%%`, compile(ast.parts[i]));
                 return res;
             })();
+
+            return compile(buildMacro(ast.value ?? "", ast.parts));
         
         case ASTType.Define:
-            return `${desmosFormat(ast.value ?? "")}(${ast.parts.slice(0, -1).map(compile).join(",")})=${compile(ast.parts.at(-1)!)}`;
+            return `${desmosFormat(ast.value ?? "")}(${ast.parts.slice(0, -1).map((v) => compile(v)).join(",")})=${compile(ast.parts.at(-1)!)}`;
 
         case ASTType.Declare:
             return `${desmosFormat(ast.value ?? "")}=${compile(ast.parts[0])}`;
@@ -87,14 +109,29 @@ export function compile(ast: AST): string {
         case ASTType.ActionDefine:
             return (() => {
                 const paramLength: number = ast.parts.findIndex((v) => v.type == ASTType.EndParameters);
-                return `${desmosFormat(ast.value ?? "")}(${ast.parts.slice(0, paramLength).map(compile).join(",")})=${ast.parts.slice(paramLength + 1).map(compile).join(",")}`;
+                return `${desmosFormat(ast.value ?? "")}(${ast.parts.slice(0, paramLength).map((v) => compile(v)).join(",")})=${ast.parts.slice(paramLength + 1).map((v) => compile(v)).join(",")}`;
             })();
         
         case ASTType.MacroDefine:
-            return ""; // TODO
+            if (!ast.value) return "";
+            
+            return (() => {
+                const paramLength: number = ast.parts.findIndex((v) => v.type == ASTType.EndParameters);
+
+                macros.set(ast.value, [
+                    ast.parts.slice(0, paramLength).map((v) => v.value!),
+                    {
+                        type: ASTType.Program,
+                        value: null,
+                        parts: ast.parts.slice(paramLength + 1)
+                    }
+                ]);
+
+                return "";
+            })();
         
         case ASTType.List:
-            return `[${ast.parts.map(compile).join(",")}]`;
+            return `[${ast.parts.map((v) => compile(v)).join(",")}]`;
         
         case ASTType.Point:
             return `(${compile(ast.parts[0])},${compile(ast.parts[1])})`;
@@ -115,5 +152,5 @@ export function compileFormat(res: string): string {
 }
 
 export function fullCompile(code: string): string {
-    return compileFormat(compile(createSyntaxTree(generateTokens(code))));
+    return compileFormat(compile(createSyntaxTree(generateTokens(code)), true));
 }
